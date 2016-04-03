8396051974057fb91d357b9e4d6abd07ead80b58
keeley <keeley.jun@qq.com>
2016-04-03 20:26:27 +0800

    添加  tornado

--- funnl.py
+++ funnl.py
@@ -1,4 +1,9 @@
-
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 from funny import app
 if __name__ == '__main__':
-    app.run(port = 8082)
+
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5000)
+    IOLoop.instance().start()
