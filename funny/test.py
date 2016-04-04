daf5ef79d704e65a69bf11c539c07793d052a1e0
keeley <keeley.jun@qq.com>
2016-04-04 20:20:46 +0800

    修改静态文件的获取方式

--- funny/test.py
+++ funny/test.py
@@ -2,14 +2,6 @@
 # -*- coding: utf-8 -*-
 # Created by tianjun on 16/2/21
 
-from funny import db
-from funny.blog_query import *
+from funny import app
 
-if __name__ == '__main__':
-    #db.create_all()
-    print selectHome()
-
-    #for item in selectRandom():
-    #    print item
-
-    #print engine.execute("select * from blog").first()
+app.run()
