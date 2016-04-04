93316ff4c111a0ab6bf562f2e6a07194d13bd50c
keeley <keeley.jun@qq.com>
2016-04-04 16:35:26 +0800

    修改静态文件的获取方式

--- funny/__init__.py
+++ funny/__init__.py
@@ -5,7 +5,7 @@
 
 from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
+app = Flask(__name__,static_folder='static', static_url_path='')
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123456@localhost/funny'
